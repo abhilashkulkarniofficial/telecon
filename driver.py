34635ff84514aafc5080c96f8b1a2af9a7fcb785
Abhilash Kulkarni <abhilashkulkarniofficial@gmail.com>
2021-12-19 21:59:51 -0800

    Add add_plots method

--- driver.py
+++ driver.py
@@ -76,6 +76,30 @@ class BoundControlBox(wx.Panel):
 
     def manual_value(self):
         return self.value
+    
+class Values(wx.Panel):
+    def __init__(self, parent, ID, label, color):
+        wx.Panel.__init__(self, parent, ID)
+        
+        box = wx.StaticBox(self, -1, label)
+        sizer = wx.StaticBoxSizer(box, wx.VERTICAL)
+        self.expected = wx.StaticText(self, label ="Expected Value:")
+        self.expected.SetForegroundColour(color)
+        # self.actual = wx.StaticText(self, label ="Actual Value:")
+        
+        sizer.Add(self.expected, 0, wx.ALL, 10)
+        # sizer.Add(self.actual, 0, wx.ALL, 10)
+
+        self.SetSizer(sizer)
+        sizer.Fit(self)
+        
+    def update_values(self, expected, actual):
+        self.expected.SetLabel(expected)
+        # self.actual.SetLabel("Actual Value:"+actual)
+        
+    def set_textColor(self, color):
+        self.expected.SetForegroundColour(color)
+
 
 
 class GraphFrame(wx.Frame):
@@ -85,19 +109,17 @@ class GraphFrame(wx.Frame):
     title ='Telecon Module'
 
     def __init__(self):
-        wx.Frame.__init__(self, None, -1, self.title, size=(600,500))
-
+        wx.Frame.__init__(self, None, -1, self.title, size=(600,500))      
+        self.data = []  
+        self.color =[]
+        self.name = []
+        
+    def add_plots(self, color, name):
         self.datagen = RandomDataGen()
-        self.data = [self.datagen.next()]
-        self.paused = False
-
-        self.create_menu()
-        self.create_status_bar()
-        self.create_main_panel()
-
-        self.redraw_timer = wx.Timer(self)
-        self.Bind(wx.EVT_TIMER, self.on_redraw_timer, self.redraw_timer)
-        self.redraw_timer.Start(1000)
+        self.color.append(color)
+        self.data.append([self.datagen.next()])
+        self.name.append(name)
+        
 
     def create_menu(self):
         self.menubar = wx.MenuBar()
@@ -122,6 +144,9 @@ class GraphFrame(wx.Frame):
         self.xmax_control = BoundControlBox(self.panel, -1, "X max", 50)
         self.ymin_control = BoundControlBox(self.panel, -1, "Y min", 0)
         self.ymax_control = BoundControlBox(self.panel, -1, "Y max", 100)
+        self.val_box = []
+        for i,x in enumerate(self.name):
+            self.val_box.append(Values(self.panel, -1, x, self.color[i]))
 
         self.pause_button = wx.Button(self.panel, -1, "Pause")
         self.Bind(wx.EVT_BUTTON, self.on_pause_button, self.pause_button)
@@ -135,12 +160,10 @@ class GraphFrame(wx.Frame):
         self.Bind(wx.EVT_CHECKBOX, self.on_cb_xlab, self.cb_xlab)
         self.cb_xlab.SetValue(True)
         
-        self.st = wx.StaticText(self.panel, label ="Values")
-        
-        self.sld = wx.Slider(self.panel, value = 10, minValue = 1, maxValue = 100,style = wx.SL_HORIZONTAL|wx.SL_LABELS)
-        
-        refresh = ["Seconds","MS"] 
-        self.combo = wx.ComboBox(self.panel,choices = refresh)
+                
+        self.refresh = {"1 Sec":1000, "100 MSec":100, "10 MSec":10, "1 MSec":1} 
+        print(self.refresh.keys())
+        self.combo = wx.ComboBox(self.panel,choices = list(self.refresh.keys()), value="Refresh Rate", style=wx.CB_READONLY)
 
         self.hbox1 = wx.BoxSizer(wx.HORIZONTAL)
         self.hbox1.Add(self.pause_button, border=5, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL)
@@ -149,12 +172,17 @@ class GraphFrame(wx.Frame):
         self.hbox1.AddSpacer(10)
         self.hbox1.Add(self.cb_xlab, border=5, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL)
         self.hbox1.AddSpacer(10)
-        self.hbox1.Add(self.st, border=5, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL)
-        self.hbox1.AddSpacer(10)
-        self.hbox1.Add(self.sld,1,flag = wx.ALL | wx.ALIGN_CENTER_VERTICAL | wx.TOP, border = 20) 
         # self.sld.Bind(wx.EVT_SLIDER, self.OnSliderScroll)
         self.hbox1.Add(self.combo,1,flag = wx.ALL | wx.ALIGN_CENTER_VERTICAL | wx.TOP, border = 5) 
 
+        self.vbox1 = wx.BoxSizer(wx.VERTICAL)
+        for x in self.val_box:
+            self.vbox1.Add(x, border=5, flag=wx.ALL)
+        
+        self.vbox2 = wx.BoxSizer(wx.HORIZONTAL)
+        self.vbox2.Add(self.canvas, 0, flag=wx.ALIGN_LEFT | wx.TOP)
+        self.vbox2.Add(self.vbox1, 0, flag=wx.ALIGN_LEFT | wx.TOP)
+        
         self.hbox2 = wx.BoxSizer(wx.HORIZONTAL)
         self.hbox2.Add(self.xmin_control, border=5, flag=wx.ALL)
         self.hbox2.Add(self.xmax_control, border=5, flag=wx.ALL)
@@ -163,33 +191,23 @@ class GraphFrame(wx.Frame):
         self.hbox2.Add(self.ymax_control, border=5, flag=wx.ALL)
 
         self.vbox = wx.BoxSizer(wx.VERTICAL)
-        self.vbox.Add(self.canvas, 1, flag=wx.LEFT | wx.TOP | wx.GROW)
-        self.vbox.Add(self.hbox1, 0, flag=wx.ALIGN_LEFT | wx.TOP)
-        self.vbox.Add(self.hbox2, 0, flag=wx.ALIGN_LEFT | wx.TOP)
+        self.vbox.Add(self.vbox2, 1, flag=wx.LEFT | wx.TOP | wx.GROW, border = 10)
+        self.vbox.Add(self.hbox1, 0, flag=wx.ALIGN_LEFT | wx.TOP, border = 10)
+        self.vbox.Add(self.hbox2, 0, flag=wx.ALIGN_LEFT | wx.TOP, border = 10)
 
         self.panel.SetSizer(self.vbox)
         self.vbox.Fit(self)
         self.combo.Bind(wx.EVT_COMBOBOX, self.OnCombo)
-        
-    def OnSliderScroll(self, e): 
-      obj = e.GetEventObject() 
-      val = obj.GetValue() 
-      font = self.GetFont() 
-      font.SetPointSize(self.sld.GetValue()) 
-      self.txt.SetFont(font) 
       
     def OnCombo(self, event): 
-        if(self.combo.GetValue()=="Seconds"):
-            self.redraw_timer.Start(1000)
-        else:
-            self.redraw_timer.Start(100)
+        self.redraw_timer.Start(self.refresh[self.combo.GetValue()])
         
     def create_status_bar(self):
         self.statusbar = self.CreateStatusBar()
 
     def init_plot(self):
         self.dpi = 100
-        self.fig = Figure((3.0, 3.0), dpi=self.dpi)
+        self.fig = Figure((6.0, 3.0), dpi=self.dpi)
 
         self.axes = self.fig.add_subplot(111)
         self.axes.set_facecolor('black')
@@ -198,13 +216,15 @@ class GraphFrame(wx.Frame):
         pylab.setp(self.axes.get_xticklabels(), fontsize=8)
         pylab.setp(self.axes.get_yticklabels(), fontsize=8)
 
-        self.plot_data = self.axes.plot( self.data, linewidth=1, color=(1, 1, 0), )[0]
+        self.plot_data = []
+        for i,data in enumerate(self.data):
+            self.plot_data.append(self.axes.plot( data, linewidth=1, color=self.color[i], )[0])
 
     def draw_plot(self):
         """ Redraws the plot
         """
         if self.xmax_control.is_auto():
-            xmax = len(self.data) if len(self.data) > 50 else 50
+            xmax = len(self.data[0]) if len(self.data[0]) > 50 else 50
         else:
             xmax = int(self.xmax_control.manual_value())
 
@@ -214,12 +234,12 @@ class GraphFrame(wx.Frame):
             xmin = int(self.xmin_control.manual_value())
 
         if self.ymin_control.is_auto():
-            ymin = round(min(self.data), 0) - 1
+            ymin = round(min(self.data[0]), 0) - 1
         else:
             ymin = int(self.ymin_control.manual_value())
 
         if self.ymax_control.is_auto():
-            ymax = round(max(self.data), 0) + 1
+            ymax = round(max(self.data[0]), 0) + 1
         else:
             ymax = int(self.ymax_control.manual_value())
 
@@ -234,9 +254,11 @@ class GraphFrame(wx.Frame):
         pylab.setp(self.axes.get_xticklabels(),
             visible=self.cb_xlab.IsChecked())
 
-        self.plot_data.set_xdata(np.arange(len(self.data)))
-        self.plot_data.set_ydata(np.array(self.data))
-        self.st.SetLabel("Value: "+str(round(self.data[len(self.data)-1],3)))
+        for i,plot in enumerate(self.plot_data):
+            plot.set_xdata(np.arange(len(self.data[i])))
+            plot.set_ydata(np.array(self.data[i]))
+            self.val_box[i].update_values(str( round(self.data[i][len(self.data[i])-1],3)), "0")
+        # self.st.SetLabel("Value: "+str( round(self.data[0][len(self.data[0])-1],3)))
 
         self.canvas.draw()
 
@@ -265,7 +287,8 @@ class GraphFrame(wx.Frame):
 
     def on_redraw_timer(self, event):
         if not self.paused:
-            self.data.append(self.datagen.next())
+            for x in self.data:
+                x.append(self.datagen.next())
 
         self.draw_plot()
 
@@ -281,7 +304,16 @@ class GraphFrame(wx.Frame):
     def on_flash_status_off(self, event):
         self.statusbar.SetStatusText('')
         
-    def set_params(self, ylabel, refresh_rate):
+    def set_chart_params(self, ylabel):
+        self.paused = False
+
+        self.create_menu()
+        self.create_status_bar()
+        self.create_main_panel()
+
+        self.redraw_timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.on_redraw_timer, self.redraw_timer)
+        self.redraw_timer.Start(1000)
         self.axes.set_ylabel(ylabel)
         self.axes.set_xlabel("Time")
 
@@ -289,6 +321,8 @@ class GraphFrame(wx.Frame):
 if __name__ == '__main__':
     app = wx.App()
     app.frame = GraphFrame()
-    app.frame.set_params("Lambda", "ms")
+    app.frame.add_plots("blue", name="Force")
+    app.frame.add_plots("red", name="Tension")
+    app.frame.set_chart_params("Values")
     app.frame.Show()
     app.MainLoop()
